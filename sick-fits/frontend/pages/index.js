14d47771614b53662562c6636e04fb2a3066b3c6
Jared <jared@digitalnutt.com>
2019-05-29 15:07:21 -0700

    init commit

---
+++ sick-fits/frontend/pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Link from 'next/link'
+
+class Home extends React.Component {
+  render() {
+    return (
+      <div>
+      </div>
+    )
+  }
+}
+
+export default Home
---
+++ sick-fits/frontend/pages/sell.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+class Sell extends React.Component {
+	render() {
+		return <p>Hey!</p>
+	}
+}
+
+export default Sell
+
+
